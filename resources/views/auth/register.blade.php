1b34fa6335f7bbc91703a1afdd4ddd988ea4d552
Motzz <s160419007@student.ubaya.ac.id>
2022-02-21 14:45:47 +0700

    Co-authored-by: LogionStevandie <LogionStevandie@users.noreply.github.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,7 +29,7 @@
                             <label for="nik" class="col-md-4 col-form-label text-md-end">NIK</label>
 
                             <div class="col-md-6">
-                                <input id="nik" type="text" class="form-control @error('nik') is-invalid @enderror" name="nik" value="{{ old('nik') }}" required autocomplete="nik">
+                                <input id="nik" type="text" class="form-control @error('nik') is-invalid @enderror" name="email" value="{{ old('nik') }}" required autocomplete="nik">
 
                                 @error('nik')
                                     <span class="invalid-feedback" role="alert">
