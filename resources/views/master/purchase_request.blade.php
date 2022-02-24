e1c50bfbd61b010d1ddaa77547c680e434051615
Motzz <s160419007@student.ubaya.ac.id>
2022-02-24 15:42:35 +0700

    Co-authored-by: LogionStevandie <LogionStevandie@users.noreply.github.com>

--- resources/views/master/purchase_request.blade.php
+++ resources/views/master/purchase_request.blade.php
@@ -45,11 +45,11 @@
                               <td>{{$pete->tanggalDiterima}}</td>
                               <td>{{$pete->email}}</td>
                               <td>  
-                               <a href="{{route('supplier.edit',[$pete->id])}}" class="btn btn-primary btn-responsive">Edit </a>
+                               <a href="{{route('purchase_request.edit',[$pete->id])}}" class="btn btn-primary btn-responsive">Edit </a>
                                   <form action="{{route('supplier.destroy',[$pete->id])}}" method="POST" class="btn btn-responsive">
                                     @csrf
                                     @method('DELETE')
-                                    <button action="{{route('supplier.destroy',[$pete->id])}}" method="POST" class="btn btn-secondary btn-danger">                          
+                                    <button action="{{route('purchase_request.destroy',[$pete->id])}}" method="POST" class="btn btn-secondary btn-danger">                          
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
                                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
                                     </svg>
---
+++ resources/views/master/purchase_request_tambah.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.home_master')
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<style>
+    p {
+        font-family: "Nunito", sans-serif;
+    }
+</style>
+@section('content')
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"></h1>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{route('barang.store')}}" method="POST">
+                            @csrf
+
+                            
+                           <div class="form-group" >
+                                <label for="Satuan">Gudang</label>
+                                <select name="gudang" class="form-control">
+                                    <option value="">
+                                        --Pilih gudang--
+                                    </option>
+                                </select>
+                                <br />
+                                
+                               
+                            </div>
+                            <div class="form-group"  id='tmbhBarang'>
+                                
+                                <select name="name[]" class="form-control" id="barang">
+                                    <option value="">
+                                        --Pilih barang--
+                                    </option>
+                                </select>
+                                <input type="number" class="form-control" placeholder="Jumlah barang" aria-label="Recipient's username" 
+                                aria-describedby="basic-addon2"id="angka" /> <br>
+                        
+                            </div>
+                            
+                        
+                 
+                            
+                            <div class="form-group" >
+
+                                <button class="btn btn-primary" type="button"id="tambah">+</button>
+
+                                <button class="btn btn-primary" type="button" id="kurang">-</button><br><br>
+                        
+                                <button class="btn btn-primary">Selesai</button>
+                            </div>
+                           
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<script type="text/javascript">
+    var tambahCombo = "";
+    var totalTambah = 0;
+    $("body").on("click", "#tambah", function () {
+        totalTambah++;
+        tambahCombo="<select name='barang[]' class='form-control'id='barang"+totalTambah+"'><option value=''> --Pilih Barang--</option></select><br>";
+
+ 
+        $('#tmbhBarang').append(tambahCombo);
+    });
+    $("body").on("click", "#kurang", function () {
+        $('#barang'+ totalTambah).remove();//i
+        totalTambah--;
+    });
+</script>
+@endsection       
