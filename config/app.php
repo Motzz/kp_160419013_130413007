1113f482de2b505f14d9e25b614a5cc383566860
Motzz <s160419007@student.ubaya.ac.id>
2022-03-31 17:34:54 +0700

    Merge branch 'try-change-users' of https://github.com/Motzz/kp_160419013_130413007

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Barryvdh\DomPDF\ServiceProvider::class,//buat pdf
+        //Barryvdh\DomPDF\ServiceProvider::class,//buat pdf
 
         /*
          * Package Service Providers...
